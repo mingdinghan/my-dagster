08502556e77079cf4b0122381ae9075766d75501
Jay Zern Ng <jayzern@hotmail.com>
2024-11-24 18:46:25 -0500

    remove airbyte assets

--- analytics/__init__.py
+++ analytics/__init__.py
@@ -4,14 +4,14 @@ from analytics.jobs import run_weather_etl
 from analytics.resources import PostgresqlDatabaseResource
 from analytics.schedules import weather_etl_schedule
 from analytics.assets import weather
-from analytics.assets.airbyte.airbyte import airbyte_assets
+# from analytics.assets.airbyte.airbyte import airbyte_assets
 from analytics.assets.dbt.dbt import dbt_warehouse, dbt_warehouse_resource
 
 
 weather_assets = load_assets_from_package_module(weather, group_name="weather")
 
 defs = Definitions(
-    assets=[*weather_assets, airbyte_assets, dbt_warehouse],
+    assets=[*weather_assets, dbt_warehouse],
     jobs=[run_weather_etl],
     schedules=[weather_etl_schedule],
     resources={
